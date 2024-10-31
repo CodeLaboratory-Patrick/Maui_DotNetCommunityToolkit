9547c4400ab3b66aeda311a2a70266e8ff012949
Donghwi_Park <gptnc5100@gmail.com>
2024-10-31 09:27:27 +0100

    Add project files.

---
+++ DotNetCommunityToolkit/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetCommunityToolkit
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
