67cdb18a8321028983b40bb7b6bb16b4c1058033
Patrick_Donghwi_Park <gptnc5100@gmail.com>
2024-10-31 10:17:25 +0100

    MVVM Toolkit - RelayCommand

--- DotNetCommunityToolkit/MVVM/ViewModels/DemoViewModel.cs
+++ DotNetCommunityToolkit/MVVM/ViewModels/DemoViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows.Input;
 using Bogus;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using DotNetCommunityToolkit.MVVM.Models;
 
 namespace DotNetCommunityToolkit.MVVM.ViewModels
@@ -18,20 +19,20 @@ namespace DotNetCommunityToolkit.MVVM.ViewModels
             set => _password = value;
         }*/
 
-        public ICommand GenerateData =>
-             new Command(() =>
-             {
-                 var faker = new Faker<FakePerson>()
-                   .RuleFor(p => p.FirstName, f => f.Person.FirstName)
-                   .RuleFor(p => p.LastName, f => f.Person.LastName)
-                   .Generate();
+        [RelayCommand]
+        async Task GenerateDate(string demo)
+        {
+            var faker = new Faker<FakePerson>()
+                .RuleFor(p => p.FirstName, f => f.Person.FirstName)
+                .RuleFor(p => p.LastName, f => f.Person.LastName)
+                .Generate();
 
-                 Person.FirstName = faker.FirstName;
-                 Person.LastName = faker.LastName;
+            Person.FirstName = faker.FirstName;
+            Person.LastName = faker.LastName;
 
-                 Password = new Faker().Internet.Password(5, true);
-             });
-
+            Password = new Faker().Internet.Password(5, true);  
+        }
+        
         public DemoViewModel()
         {
             Person = new FakePerson
